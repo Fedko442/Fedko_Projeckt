559d3fd55490c6b68e90e703166d2758690a1d48
Fedko442 <makfedkomakfedko@gmail.com>
2025-09-27 22:46:56 +0300

    message commit

--- PZ _3/PZ_3.2.py
+++ PZ _3/PZ_3.2.py
@@ -1,10 +1,10 @@
 try:
-    a, b = int(input('Введите первое число: ')), int(input('Введите второе число '))
-    if a > b:
+    a, b = int(input('Введите первое число: ')), int(input('Введите второе число ')) #запрос чисел
+    if a > b: #проверка какое число больше
         print(a, b)
     elif b > a:
         print(b, a)
-    else:
-        print('Ошибка!')
+    else: #если числа равны
+        print('Числа равны!')
 except ValueError:
     print('Ошибка!')
