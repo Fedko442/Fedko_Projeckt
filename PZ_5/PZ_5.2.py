4bfb98c0c01bc90b5f25e49f673c0b24163a4788
Fedko442 <makfedkomakfedko@gmail.com>
2025-11-08 15:07:26 +0300

    commit message

--- PZ_5/PZ_5.2.py
+++ PZ_5/PZ_5.2.py
@@ -1,13 +1,14 @@
-#Напишите функцию digit_count_sum(k), которая принимает целое положительное число k и выводит на экран:
 def digit_count_sum(k):
-    s = str(k)
-    count = len(s)
+    count = 0
     total = 0
-    for char in s:
-        total += int(char)
+    temp = k
+    while temp > 0:
+        total += temp % 10
+        temp //= 10
+        count += 1
     print(f"Число: {k}, количество цифр: {count}, сумма цифр: {total}")
 
-# Проверка для пяти чисел
+# Проверка:
 numbers = [123, 4567, 89, 1000, 7]
 for n in numbers:
     digit_count_sum(n)
