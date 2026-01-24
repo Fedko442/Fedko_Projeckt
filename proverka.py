c1f3e0a7500544273ab3dce46cc0f3e2277d2d2a
Fedko442 <makfedkomakfedko@gmail.com>
2026-01-24 12:51:01 +0300

    commit message

---
+++ proverka.py
@@ -0,0 +1,14 @@
+sampleData = {
+    "class": {
+        "student": {
+            "name":"Mike",
+            "marks": {
+                'physics': 70,
+                "history": 80
+            }
+        }
+    }
+}
+
+print(sampleData['class']['student']['marks']['history'])
+
