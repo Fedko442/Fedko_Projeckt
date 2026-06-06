92f4c687d04ba6920dc1bfb0a1d288c2412f5833
Fedko442 <makfedkomakfedko@gmail.com>
2026-06-06 16:17:46 +0300

    commit message

--- PZ_13/PZ_13.1.py
+++ PZ_13/PZ_13.1.py
@@ -1,20 +1,27 @@
 try:
     with open("writer.txt", "r", encoding="utf-8") as file:
-        text = file.read()
+        lines = file.readlines()
 
-    words = text.split()
-
-    surnames = list(filter(lambda word: word.istitle(), words))
+    surnames = list(
+        map(
+            lambda line: line.split()[0],
+            filter(lambda line: line.strip(), lines)
+        )
+    )
 
     print("Фамилии писателей:")
     print(*surnames, sep="\n")
 
-    print("Количество фамилий:", len(surnames))
+    print("\nКоличество фамилий:", len(surnames))
+
+    text = ''.join(lines)
 
     new_text = text.replace("роман", "произведение")
 
     with open("new_writer.txt", "w", encoding="utf-8") as file:
         file.write(new_text)
+
+    print("Файл new_writer.txt создан")
 
 except FileNotFoundError:
     print("Файл writer.txt не найден")
