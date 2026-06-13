eb20f2fe5d1258fa76a94eb460533a06bba21b72
Fedko442 <makfedkomakfedko@gmail.com>
2026-06-13 10:25:01 +0300

    commit message

---
+++ PZ_16/PZ16.2.py
@@ -0,0 +1,37 @@
+#Создание базового класса «Работник» и его наследование для создания классов «Менеджер» и «Инженер». 
+# В классе «Работник» будут общие методы, такие как «работать» и «получать зарплату», 
+# а классы-наследники будут иметь свои уникальные методы и свойства, 
+# такие как «управлять командой» и «проектировать системы».
+
+class Employee:
+
+    def __init__(self, name):
+        self.name = name
+
+    def work(self):
+        print(self.name, "работает")
+
+    def get_salary(self):
+        print(self.name, "получает зарплату")
+
+
+class Manager(Employee):
+
+    def manage_team(self):
+        print(self.name, "управляет командой")
+
+class Engineer(Employee):
+
+    def design_systems(self):
+        print(self.name, "проектирует системы")
+
+manager = Manager("Иван")
+engineer = Engineer("Петр")
+
+manager.work()
+manager.get_salary()
+manager.manage_team()
+
+engineer.work()
+engineer.get_salary()
+engineer.design_systems()
